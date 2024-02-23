b85bd3fdbe1e82852a2621d0799a91dc054012ca
Alex Olsen <alexnolsen@gmail.com>
2024-02-22 16:59:35 -0800

    set up user auth

--- piggybank/src/components/shared/Header.tsx
+++ piggybank/src/components/shared/Header.tsx
@@ -1,15 +1,35 @@
-import Link from 'next/link';
-import React from 'react';
+import Link from "next/link";
+import Image from "next/image";
+import React from "react";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { Button } from "../ui/button";
 
 const Header = () => {
-    return (
-       <header className='w-full border-b '>
-        <div className='wrapper flex items-center justify-between'>
-            <Link href='/'></Link>
+  return (
+    <header className="w-full border-b ">
+      <div className="wrapper flex items-center justify-between">
+        <Link href="/" className="w-36">
+          <Image
+            src="/assets/logo/piggybankIconNoBG.png"
+            width={48}
+            height={48}
+            alt="logo"
+          />
+        </Link>
 
+        <div className="flex w-32 justify-end gap-3">
+          <SignedIn>
+            <UserButton afterSignOutUrl="/" />
+          </SignedIn>
+          <SignedOut>
+            <Button asChild className="rounded-full" size="lg">
+              <Link href="/sign-in">Login</Link>
+            </Button>
+          </SignedOut>
         </div>
-       </header>
-    );
+      </div>
+    </header>
+  );
 };
 
 export default Header;
